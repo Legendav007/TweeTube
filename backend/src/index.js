3c01854b6e59190061288b0fcacf874d81d22ac2
Abhinav Verma <legendav@kali>
2025-04-19 17:26:48 +0530

    backend-initiate

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDB from "./db/db_connect.js";
+
+
+dotenv.config({
+    path : "../.env"
+})
+// console.log("MongoDB url : ", process.env.MONGODB_URI);
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Serves is running on ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Database connection failed!!" , err);
+})
