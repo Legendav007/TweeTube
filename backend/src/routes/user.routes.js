efad2fa791fa48ee85171517c02958f63b0a95f5
Abhinav Verma <legendaryav007@gmail.com>
2025-06-27 06:44:12 +0530

    testing ssh key

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import {Router} from "express"
-import {logoutUser, registerUser , loginUser , refreshAccessToken , changeCurrentPassword , getCurrentUser} from "../controllers/user.controller.js"
+import {logoutUser, registerUser , loginUser , refreshAccessToken , changeCurrentPassword , getCurrentUser , updateAccountDetails , updateUserAvatar
+    ,updateUserCoverImage ,getUserChannelProfile ,getWatchHistory
+} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -27,5 +29,10 @@ router.route("/logout").post(verifyJWT , logoutUser)
 router.route(refreshAccessToken)
 router.route("/change-password").post(verifyJWT , changeCurrentPassword)
 router.route("/current-user").get(verifyJWT , getCurrentUser)
+router.route("/update-account").patch(verifyJWT , updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar") , updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT , upload.single("coverImage") , updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT , getWatchHistory)
 
 export default router;
